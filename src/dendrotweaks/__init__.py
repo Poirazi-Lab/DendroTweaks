4b6cad80c69c6936a21f7334610617609ab20a88
Roman Makarov <sendittoroman@gmail.com>
2025-03-05 12:27:36 +0200

    Copy source code

---
+++ src/dendrotweaks/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+
+from dendrotweaks.model import Model
+from dendrotweaks.simulators import NEURONSimulator
+from dendrotweaks.membrane.distributions import Distribution
+from dendrotweaks.membrane.groups import SectionGroup
+from dendrotweaks.path_manager import PathManager
+from dendrotweaks.stimuli import Synapse, Population, IClamp
