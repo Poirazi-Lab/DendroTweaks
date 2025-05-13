17a255ba8ae53f6d75c8567276964a3354d0c8d2
Roman Makarov <sendittoroman@gmail.com>
2025-05-14 01:55:36 +0300

    Update Jaxley template

--- examples/Test/biophys/python/stdNa.py
+++ examples/Test/biophys/python/stdNa.py
@@ -4,7 +4,7 @@
 from dendrotweaks.biophys.mechanisms import IonChannel
 from jaxley.channels import Channel
 from jaxley.solver_gate import exponential_euler
-import jax.numpy as jn 
+import jax.numpy as np
 
 class stdNa(Channel, IonChannel):
     """
@@ -14,28 +14,58 @@ class stdNa(Channel, IonChannel):
     def __init__(self, name="stdNa"):
         self.current_is_in_mA_per_cm2 = True
         super().__init__(name=name)
+        self.params = {
+            "vhalf_m": -32.571,
+            "sigma_m": 9.8,
+            "k_m": 1.882,
+            "delta_m": 0.541,
+            "tau0_m": 0.065,
+            "vhalf_h": -60.0,
+            "sigma_h": -6.2,
+            "k_h": 0.018,
+            "delta_h": 0.395,
+            "tau0_h": 0.797,
+            "gbar": 0.0,
+            "q10": 2.3,
+            "temp": 23
+            }
         self.channel_params = {
-            "stdNa_vhalf_m": -32.571,
-            "stdNa_sigma_m": 9.8,
-            "stdNa_k_m": 1.882,
-            "stdNa_delta_m": 0.541,
-            "stdNa_tau0_m": 0.065,
-            "stdNa_vhalf_h": -60.0,
-            "stdNa_sigma_h": -6.2,
-            "stdNa_k_h": 0.018,
-            "stdNa_delta_h": 0.395,
-            "stdNa_tau0_h": 0.797,
-            "stdNa_gbar": 0.0,
-            "stdNa_q10": 2.3,
-            "stdNa_temp": 23
+            "vhalf_m_stdNa": -32.571,
+            "sigma_m_stdNa": 9.8,
+            "k_m_stdNa": 1.882,
+            "delta_m_stdNa": 0.541,
+            "tau0_m_stdNa": 0.065,
+            "vhalf_h_stdNa": -60.0,
+            "sigma_h_stdNa": -6.2,
+            "k_h_stdNa": 0.018,
+            "delta_h_stdNa": 0.395,
+            "tau0_h_stdNa": 0.797,
+            "gbar_stdNa": 0.0,
+            "q10_stdNa": 2.3,
+            "temp_stdNa": 23
+            }
+        self.range_params = {
+            "vhalf_m": -32.571,
+            "sigma_m": 9.8,
+            "k_m": 1.882,
+            "delta_m": 0.541,
+            "tau0_m": 0.065,
+            "vhalf_h": -60.0,
+            "sigma_h": -6.2,
+            "k_h": 0.018,
+            "delta_h": 0.395,
+            "tau0_h": 0.797,
+            "gbar": 0.0,
+            "q10": 2.3,
+            "temp": 23
             }
         self.channel_states = {
-            "stdNa_m": 0.0,
-            "stdNa_h": 0.0
+            "m_stdNa": 0.0,
+            "h_stdNa": 0.0
             }
         self._state_powers = {
-            "stdNa_m": {'power': 3},
-            "stdNa_h": {'power': 1}
+            "m_stdNa": {'power': 3},
+            "h_stdNa": {'power': 1}
             }
         self.ion = "na"
         self.current_name = "i_na"
@@ -63,16 +93,16 @@ class stdNa(Channel, IonChannel):
         return beta_prime
     
     def compute_kinetic_variables(self, v):
-        vhalf_m = self.channel_params.get("stdNa_vhalf_m", 1)
-        sigma_m = self.channel_params.get("stdNa_sigma_m", 1)
-        k_m = self.channel_params.get("stdNa_k_m", 1)
-        delta_m = self.channel_params.get("stdNa_delta_m", 1)
-        tau0_m = self.channel_params.get("stdNa_tau0_m", 1)
-        vhalf_h = self.channel_params.get("stdNa_vhalf_h", 1)
-        sigma_h = self.channel_params.get("stdNa_sigma_h", 1)
-        k_h = self.channel_params.get("stdNa_k_h", 1)
-        delta_h = self.channel_params.get("stdNa_delta_h", 1)
-        tau0_h = self.channel_params.get("stdNa_tau0_h", 1)
+        vhalf_m = self.channel_params.get("vhalf_m_stdNa", 1)
+        sigma_m = self.channel_params.get("sigma_m_stdNa", 1)
+        k_m = self.channel_params.get("k_m_stdNa", 1)
+        delta_m = self.channel_params.get("delta_m_stdNa", 1)
+        tau0_m = self.channel_params.get("tau0_m_stdNa", 1)
+        vhalf_h = self.channel_params.get("vhalf_h_stdNa", 1)
+        sigma_h = self.channel_params.get("sigma_h_stdNa", 1)
+        k_h = self.channel_params.get("k_h_stdNa", 1)
+        delta_h = self.channel_params.get("delta_h_stdNa", 1)
+        tau0_h = self.channel_params.get("tau0_h_stdNa", 1)
         
         mInf = 1 / (1 + np.exp((-(v - vhalf_m) / sigma_m)))
         alpha_m = self.alpha_prime(v, k_m, delta_m, vhalf_m, sigma_m)
@@ -85,20 +115,20 @@ class stdNa(Channel, IonChannel):
         return mInf, mTau, hInf, hTau
 
     def update_states(self, states, dt, v, params):
-        m = states['stdNa_m']
-        h = states['stdNa_h']
+        m = states['m_stdNa']
+        h = states['h_stdNa']
         mInf, mTau, hInf, hTau = self.compute_kinetic_variables(v)
         new_m = exponential_euler(m, dt, mInf, mTau)
         new_h = exponential_euler(h, dt, hInf, hTau)
         return {
-            "stdNa_m": new_m,
-            "stdNa_h": new_h
+            "m_stdNa": new_m,
+            "h_stdNa": new_h
             }
 
     def compute_current(self, states, v, params):
-        m = states['stdNa_m']
-        h = states['stdNa_h']
-        gbar = params["stdNa_gbar"]
+        m = states['m_stdNa']
+        h = states['h_stdNa']
+        gbar = params["gbar_stdNa"]
         # E = params["E_na"]
         E = 60
         mInf, mTau, hInf, hTau = self.compute_kinetic_variables(v)
@@ -108,7 +138,7 @@ class stdNa(Channel, IonChannel):
     def init_state(self, states, v, params, delta_t):
         mInf, mTau, hInf, hTau = self.compute_kinetic_variables(v)
         return {
-            "stdNa_m": mInf,
-            "stdNa_h": hInf
+            "m_stdNa": mInf,
+            "h_stdNa": hInf
             }
 
